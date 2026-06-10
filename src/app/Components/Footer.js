540182bb016f96b757c45886445d2657ba65f7d3
codaaiteam <codaaiteam@gmail.com>
2026-06-10 11:09:55 -0400

    Add game footer links

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -112,6 +112,23 @@ const Footer = () => {
 
 
 
+        <span>·</span>
+        <a href="https://38-0-game.com/" target="_blank" rel="noopener noreferrer">
+        38-0 Game
+        </a>
+        <span>·</span>
+        <a href="https://www.82-0-challenge.com/" target="_blank" rel="noopener noreferrer">
+        82-0 Challenge
+        </a>
+        <span>·</span>
+        <a href="https://7-0-game.com/" target="_blank" rel="noopener noreferrer">
+        7-0 Game
+        </a>
+        <span>·</span>
+        <a href="https://17-0-game.com/" target="_blank" rel="noopener noreferrer">
+        17-0 Challenge
+        </a>
+
     </footer>
   );
 };
